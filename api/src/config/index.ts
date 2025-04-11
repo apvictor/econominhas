e0cd7a81e653f8802c5a6158ecae76970ba5a913
apvictor <armando2019ti@gmail.com>
2025-04-11 11:38:24 -0300

    refactor: remover constantes CLASSE_ADOLESCENTE e CLASSE_GVJ do arquivo de configuração

--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -1,7 +1,5 @@
 import { Prisma } from '../shared/services/prisma';
 
-export const CLASSE_ADOLESCENTE = 4;
-export const CLASSE_GVJ = 5;
 export const JWT_SECRET = process.env.JWT_SECRET as string;
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string;
